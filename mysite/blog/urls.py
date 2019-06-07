b469ecb7a5814a746e8e0ec938903e1cb774e9ca
Blairhuang <2382578705@qq.com>
2019-06-07 10:10:19 +0800

    1-change to blog
    replace wombat to blog

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('', views.home, name='blog-home'),
+
+
 	# path(r'example_get/<str:var_a>/<int:var_b>',  views.example_get),
 	# path(r'example_post/', views.example_post),
 ]
